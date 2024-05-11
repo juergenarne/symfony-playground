49e5ac4919929fe95b95c592dbe2a28d6f2720ca
Juergen Arne Klein <mail@juergen-arne-klein.de>
2024-05-11 15:23:39 +0200

    [interrim] übergabe

--- src/Controller/BootstrapController.php
+++ src/Controller/BootstrapController.php
@@ -6,11 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class LuckyController extends AbstractController
+class BootstrapController extends AbstractController
 {
     #[Route('/bootstrap/tooltip', name: 'app_bootstrap_tooltip')]
     public function tooltip(): Response
     {
-        return new Response();
+        return new Response('<h1>Hallo Welt!</h1>');
     }
 }
